1a46c57def0e6a2bed4522f5e3d24cee8e8dab65
NahuelOlgiati <nahuelolgiati@hotmail.com>
2017-06-16 12:18:54 -0300

    its f working

--- src/app/ong-fiends-circle/ong-fiends-circle.component.ts
+++ src/app/ong-fiends-circle/ong-fiends-circle.component.ts
@@ -1,68 +1,55 @@
 import { Component } from '@angular/core';
 
-class LiStyle {
+class ItemStyle {
   constructor(public left: string, public top: string, public width: string, public opacity: string) { }
 }
-class Product {
-  constructor(public name: string, public quantity: number, public cost: number, public style: LiStyle) { }
+class People {
+  constructor(public name: string, public style?: ItemStyle) { }
 }
-
+class Group {
+  constructor(public name: string, public peoples: People[]) { }
+}
 @Component({
   selector: 'ong-fiends-circle',
   templateUrl: './ong-fiends-circle.component.html',
   styleUrls: ['./ong-fiends-circle.component.scss']
 })
 export class OngFiendsCircleComponent {
-  availableProducts: Array<Product> = [];
-  shoppingBasket: Array<Product> = [];
-  circles = ['Friends', 'Family', 'Private'];
 
-  private count = 0;
+  private conf = {
+    radius: 40,
+    center: [39, 39],
+    groupItemWidth: '22px'
+  };
+  private peoples: People[] = [];
+  private groups: Group[] = [new Group('Friends', []), new Group('Family', []), new Group('Private', [])];
 
   constructor() {
-    this.availableProducts.push(new Product('Blue Shoes', 3, 35, null));
-    this.availableProducts.push(new Product('Good Jacket', 1, 90, null));
-    this.availableProducts.push(new Product('Red Shirt', 5, 12, null));
-    this.availableProducts.push(new Product('Blue Jeans', 4, 60, null));
+    this.peoples.push(new People('Blue Shoes'));
+    this.peoples.push(new People('Good Jacket'));
+    this.peoples.push(new People('Red Shirt'));
+    this.peoples.push(new People('Blue Jeans'));
   }
 
-  orderedProduct($event: any) {
-    let orderedProduct: Product = $event.dragData;
-    orderedProduct.quantity--;
-  }
-
-  addToBasket($event: any) {
-    console.log($event);
-
-    let newProduct: Product = $event.dragData;
-
-    this.count++;
-    const coord = this.coord(this.count);
-    const style = {left: coord.x, top: coord.y, width: '22px', opacity: '1'};
-    //left: 16.0569px; top: 6.23392px; width: 22px; opacity: 1;
-
-    this.shoppingBasket.push(new Product(newProduct.name, 1, newProduct.cost, style));
-    this.shoppingBasket.sort((a: Product, b: Product) => {
-      return a.name.localeCompare(b.name);
-    });
+  private addPeople($event: any, groupName: string): void {
+    const peopleDraged: People = $event.dragData;
+    for (const group of this.groups) {
+      if (group.name == groupName) {
+        const coord = this.coord(group.peoples.length + 1);
+        const style = { left: coord.x, top: coord.y, width: this.conf.groupItemWidth, opacity: '1' };
+        group.peoples.push(new People(peopleDraged.name, style));
+      }
+    }
   }
 
   private coord(count: number): any {
-    const radius = 40;
-    const center = [39, 39];
     const a = (-180 + count * 35) * 3.1415927 / 180; // to radians
-
-    const x = (Math.sin(a) * radius + center[0]).toPrecision(4) + 'px';
-    const y = (Math.cos(a) * radius + center[1]).toPrecision(4) + 'px';
+    const x = (Math.sin(a) * this.conf.radius + this.conf.center[0]).toPrecision(4) + 'px';
+    const y = (Math.cos(a) * this.conf.radius + this.conf.center[1]).toPrecision(4) + 'px';
     return { x: x, y: y };
   }
 
-  totalCost(): number {
-    let cost: number = 0;
-    for (let indx in this.shoppingBasket) {
-      let product: Product = this.shoppingBasket[indx];
-      cost += (product.cost * product.quantity);
-    }
-    return cost;
+  private getGroupsNames(): string[]{
+    return this.groups.map(function (val) { return val.name; });
   }
 }
