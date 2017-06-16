240afe2e8087c059aff378644261f70caead9056
nahuel <nahuelolgiati@hotmail.com>
2017-06-15 23:08:23 -0300

    domani

--- src/app/ong-fiends-circle/ong-fiends-circle.component.ts
+++ src/app/ong-fiends-circle/ong-fiends-circle.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
+class LiStyle {
+  constructor(public left: string, public top: string, public width: string, public opacity: string) { }
+}
 class Product {
-  constructor(public name: string, public quantity: number, public cost: number) { }
+  constructor(public name: string, public quantity: number, public cost: number, public style: LiStyle) { }
 }
 
 @Component({
@@ -14,11 +17,13 @@ export class OngFiendsCircleComponent {
   shoppingBasket: Array<Product> = [];
   circles = ['Friends', 'Family', 'Private'];
 
+  private count = 0;
+
   constructor() {
-    this.availableProducts.push(new Product('Blue Shoes', 3, 35));
-    this.availableProducts.push(new Product('Good Jacket', 1, 90));
-    this.availableProducts.push(new Product('Red Shirt', 5, 12));
-    this.availableProducts.push(new Product('Blue Jeans', 4, 60));
+    this.availableProducts.push(new Product('Blue Shoes', 3, 35, null));
+    this.availableProducts.push(new Product('Good Jacket', 1, 90, null));
+    this.availableProducts.push(new Product('Red Shirt', 5, 12, null));
+    this.availableProducts.push(new Product('Blue Jeans', 4, 60, null));
   }
 
   orderedProduct($event: any) {
@@ -30,17 +35,26 @@ export class OngFiendsCircleComponent {
     console.log($event);
 
     let newProduct: Product = $event.dragData;
-    for (let indx in this.shoppingBasket) {
-      let product: Product = this.shoppingBasket[indx];
-      if (product.name === newProduct.name) {
-        product.quantity++;
-        return;
-      }
-    }
-    this.shoppingBasket.push(new Product(newProduct.name, 1, newProduct.cost));
+
+    this.count++;
+    const coord = this.coord(this.count);
+    const style = {left: coord.x, top: coord.y, width: '22px', opacity: '1'};
+    //left: 16.0569px; top: 6.23392px; width: 22px; opacity: 1;
+
+    this.shoppingBasket.push(new Product(newProduct.name, 1, newProduct.cost, style));
     this.shoppingBasket.sort((a: Product, b: Product) => {
       return a.name.localeCompare(b.name);
     });
+  }
+
+  private coord(count: number): any {
+    const radius = 40;
+    const center = [39, 39];
+    const a = (-180 + count * 35) * 3.1415927 / 180; // to radians
+
+    const x = (Math.sin(a) * radius + center[0]).toPrecision(4) + 'px';
+    const y = (Math.cos(a) * radius + center[1]).toPrecision(4) + 'px';
+    return { x: x, y: y };
   }
 
   totalCost(): number {
